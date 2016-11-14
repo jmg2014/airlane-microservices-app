7eaecbafe8a6ac78c55c9829d5b9844830e58450
jorge <jormangon@gmail.com>
2016-11-14 23:56:03 +0000

    Fixed not found index.html

--- airlane-webapp/src/main/java/com/springcloud/myapp/SpringBootWebApplication.java
+++ airlane-webapp/src/main/java/com/springcloud/myapp/SpringBootWebApplication.java
@@ -3,21 +3,51 @@ package com.springcloud.myapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SpringBootWebApplication {
+public class SpringBootWebApplication extends WebMvcConfigurerAdapter {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringBootWebApplication.class, args);
   }
-  
+
   @LoadBalanced
   @Bean
   public RestTemplate loadRestTemplate() {
-      return new RestTemplate();
+    return new RestTemplate();
+  }
+
+  @Bean
+  public ViewResolver viewResolver() {
+    ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+    templateResolver.setTemplateMode("XHTML");
+    templateResolver.setPrefix("views/");
+    templateResolver.setSuffix(".html");
+
+    SpringTemplateEngine engine = new SpringTemplateEngine();
+    engine.setTemplateResolver(templateResolver);
+
+    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+    viewResolver.setTemplateEngine(engine);
+    return viewResolver;
+  }
+
+  @Controller
+  public class WebController {
+    @RequestMapping(value = "/")
+    public String index() {
+      return "index";
+    }
   }
 }

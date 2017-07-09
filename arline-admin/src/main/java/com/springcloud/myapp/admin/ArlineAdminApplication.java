1918ac62bcda1f0a36bed58ce012e42e8a4a8c82
Jorge Manrique <jormangon@gmail.com>
2017-07-09 10:19:22 +0100

    Added Spring Boot Actuator

--- arline-admin/src/main/java/com/springcloud/myapp/admin/ArlineAdminApplication.java
+++ arline-admin/src/main/java/com/springcloud/myapp/admin/ArlineAdminApplication.java
@@ -2,11 +2,13 @@ package com.springcloud.myapp.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
 public class ArlineAdminApplication {
 
   public static void main(String[] args) {
